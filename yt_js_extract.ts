02220c247664ae0c62886554d2fff517a0939d6a
alive4ever <alive4ever@users.noreply.github.com>
2025-11-19 05:57:54 +0000

    yt_js_extract: simplify main task
    Use `innertube.session` result to construct `sig_code` without
    extracting any video streaming data.

--- yt_js_extract.ts
+++ yt_js_extract.ts
@@ -1,22 +1,5 @@
-import { Innertube, Platform, Types } from 'youtubei.js'
+import { Innertube } from 'youtubei.js'
 
-let sig_code;
-Platform.shim.eval = async (data: Types.BuildScriptResult, env: Record<string, Types.VMPrimative>) => {
-  const properties = [];
-
-  if(env.n) {
-    properties.push(`n: exportedVars.nFunction("${env.n}")`)
-  }
-
-  if (env.sig) {
-    properties.push(`sig: exportedVars.sigFunction("${env.sig}")`)
-  }
-
-  const code = `${data.output}\nreturn { ${properties.join(', ')} }`;
-
-  sig_code = data;
-  return new Function(code)();
-}
 let innertube
 let player_id
 const args = process.argv.slice(2)
@@ -27,7 +10,6 @@ player_id = innertube.session.player.player_id
 player_id = args[0];
 innertube = await Innertube.create({'client': 'TV', 'lang': 'en', 'player_id': player_id})
 };
-var video_id = 'jNQXAC9IVRw'
-var info = await innertube.getStreamingData(video_id, '')
+const sig_code = innertube.session.player.data
 sig_code.player_id = player_id;
 console.log(JSON.stringify(sig_code));
